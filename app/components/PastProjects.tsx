f85e77e571289ea303317090caf222a808197b64
AJ Brownson <92381518+AJBrownson@users.noreply.github.com>
2025-08-22 12:27:08 +0000

    feat: Add new project

--- app/components/PastProjects.tsx
+++ app/components/PastProjects.tsx
@@ -1,62 +1,60 @@
 import Image from "next/image";
-// import Hoster from "@/public/hoster.png";
 import Rooks from "@/public/rooks.png";
-import Solgacy from "@/public/solgacy.png";
+// import Solgacy from "@/public/solgacy.png";
 import Kekius from "@/public/kekius.png";
+import Tremic from "@/public/tremic.png"
 import { FaExternalLinkAlt } from "react-icons/fa";
 import Link from "next/link";
 
 export default function Projects() {
   return (
     <main className="flex flex-col gap-y-12">
-      {/* <div className="flex flex-col-reverse lg:items-center lg:flex-row justify-between font-geistMono">
+
+      <div className="flex flex-col-reverse lg:items-center lg:flex-row justify-between font-geistMono">
         <div className="lg:w-1/2 mt-5 lg:mt-0">
           <h1 className="text-xl font-semibold mb-2 text-gray-200">
-            Hoster | Social Media Posts Scheduler
+            Tre-Mic Technologies LTD | Water Systems Installation 
           </h1>
           <p className="text-sm md:text-base text-gray-300">
-            This is a social media posts scheduler for the Twitter and Facebook
-            platforms. NextJS + TypeScript + TailwindCSS was used for the
-            frontend part, Upstash + Redis was used for the database and BullMQ
-            + Trigger.dev was used for queuing the scheduled posts.
+            This is a website built for an advanced water systems installation company. Built using NextJS + TypeScript + TailwindCSS
           </p>
           <Link
-            href="https://hoster-social.vercel.app"
+            href="https://tremictech.com/"
             target="_blank"
             rel="noopener noreferrer"
             className="lg:hidden flex justify-end"
           >
-            <p className="p-2 text-sm flex items-center gap-x-2 text-gray-300">
-              Visit Site <FaExternalLinkAlt />{" "}
+            <p className="p-2 text-sm flex items-center gap-x-2 text-white">
+              Visit Site <FaExternalLinkAlt color="white" />{" "}
             </p>
           </Link>
         </div>
 
         <div className="border border-slate-700">
           <div className="lg:w-80 h-auto">
-            <Image src={Hoster} alt="" />
+            <Image src={Tremic} alt="" />
           </div>
           <Link
-            href="https://hoster-social.vercel.app"
+            href="https://tremictech.com/"
             target="_blank"
             rel="noopener noreferrer"
             className="hidden lg:flex justify-end"
           >
-            <p className="p-2 text-sm flex items-center gap-x-2">
-              Visit Site <FaExternalLinkAlt />{" "}
+            <p className="p-2 text-sm flex items-center gap-x-2 text-white">
+              Visit Site <FaExternalLinkAlt color="white" />{" "}
             </p>
           </Link>
         </div>
-      </div> */}
+      </div>
 
-        <div className="flex flex-col-reverse lg:items-center lg:flex-row justify-between font-geistMono">
+      <div className="flex flex-col-reverse lg:items-center lg:flex-row justify-between font-geistMono">
         <div className="lg:w-1/2 mt-5 lg:mt-0">
           <h1 className="text-xl font-semibold mb-2 text-gray-200">
             Kekius XRP | Memecoin Website
           </h1>
           <p className="text-sm md:text-base text-gray-300">
-             This is a memecoin website built for the XRP blockchain. NextJS + TypeScript + TailwindCSS was
-             used to build the website.
+            This is a memecoin website built for the XRP blockchain. NextJS + TypeScript + TailwindCSS was
+            used to build the website.
           </p>
           <Link
             href="https://kekius-xrp.com/"
@@ -65,7 +63,7 @@ export default function Projects() {
             className="lg:hidden flex justify-end"
           >
             <p className="p-2 text-sm flex items-center gap-x-2 text-white">
-              Visit Site <FaExternalLinkAlt color="white"/>{" "}
+              Visit Site <FaExternalLinkAlt color="white" />{" "}
             </p>
           </Link>
         </div>
@@ -75,19 +73,19 @@ export default function Projects() {
             <Image src={Kekius} alt="" />
           </div>
           <Link
-          href="https://kekius-xrp.com/"
+            href="https://kekius-xrp.com/"
             target="_blank"
             rel="noopener noreferrer"
             className="hidden lg:flex justify-end"
           >
             <p className="p-2 text-sm flex items-center gap-x-2 text-white">
-              Visit Site <FaExternalLinkAlt color="white"/>{" "}
+              Visit Site <FaExternalLinkAlt color="white" />{" "}
             </p>
           </Link>
         </div>
       </div>
 
-      <div className="flex flex-col-reverse lg:items-center lg:flex-row justify-between font-geistMono">
+      {/* <div className="flex flex-col-reverse lg:items-center lg:flex-row justify-between font-geistMono">
         <div className="lg:w-1/2 mt-5 lg:mt-0">
           <h1 className="text-xl font-semibold mb-2 text-gray-200">
             Solgacy | Web3 Casino Gaming Website
@@ -105,7 +103,7 @@ export default function Projects() {
             className="lg:hidden flex justify-end"
           >
             <p className="p-2 text-sm flex items-center gap-x-2 text-white">
-              Visit Site <FaExternalLinkAlt color="white"/>{" "}
+              Visit Site <FaExternalLinkAlt color="white" />{" "}
             </p>
           </Link>
         </div>
@@ -121,11 +119,11 @@ export default function Projects() {
             className="hidden lg:flex justify-end"
           >
             <p className="p-2 text-sm flex items-center gap-x-2 text-white">
-              Visit Site <FaExternalLinkAlt color="white"/>{" "}
+              Visit Site <FaExternalLinkAlt color="white" />{" "}
             </p>
           </Link>
         </div>
-      </div>
+      </div> */}
 
       <div className="flex flex-col-reverse lg:items-center lg:flex-row justify-between font-geistMono">
         <div className="lg:w-1/2 mt-5 lg:mt-0">
@@ -143,7 +141,7 @@ export default function Projects() {
             className="lg:hidden flex justify-end"
           >
             <p className="p-2 text-sm flex items-center gap-x-2 text-white">
-              Visit Site <FaExternalLinkAlt color="white"/>{" "}
+              Visit Site <FaExternalLinkAlt color="white" />{" "}
             </p>
           </Link>
         </div>
@@ -159,7 +157,7 @@ export default function Projects() {
             className="hidden lg:flex justify-end"
           >
             <p className="p-2 text-sm flex items-center gap-x-2 text-white">
-              Visit Site <FaExternalLinkAlt color="white"/>{" "}
+              Visit Site <FaExternalLinkAlt color="white" />{" "}
             </p>
           </Link>
         </div>
